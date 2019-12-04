2f726b378deb128b20508d3affd7882ee7e62537
Tom Van Doorslaer <tom.vandoorslaer@fiddle.be>
2019-12-04 13:29:04 +0000

    Update Gruntfile.js, package.json files

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,23 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
-        dir: {
+        "pdfAuth":{
+            username: sUser,
+            password:sPwd
+        },
+        
+        "dir": {
             src: "webapp",
             dest: "dist"
         },
 
-        copy: {
+        //clean dist folder
+        "clean": {
+            dist: "<%= dir.dest %>/**"
+        },
+        
+        //copy from webapp to dist
+        "copy": {
             dist: {
                 files: [{
                     expand: true,
@@ -25,12 +36,9 @@ module.exports = function(grunt) {
                 }]
             }
         },
-
-        clean: {
-            dist: "<%= dir.dest %>/**"
-        },
         
-        openui5_preload: {
+        //preload generation
+        "openui5_preload": {
             component: {
                 options: {
                     resources: {
@@ -55,18 +63,60 @@ module.exports = function(grunt) {
                 components: "be/fiddle/sitbe"
             }
         },
+
+        "ftp-deploy": {
+            build: {
+                auth: {
+                    host: 'fiddle.be',
+                    port: 21,
+                    authKey: 'pdfAuth'
+                },
+                src: './dist',
+                dest: '/sitbe/',
+                exclusions: []
+            }
+        }
     });
+
+	//JSDoco parsing
+	grunt.registerTask('jsdoc', 'parsing all jsdoco into one big json file', function(){
+		grunt.log.writeln('Generating JSDoc file');
+		var done = this.async();
+
+		var jsdocx = require('jsdoc-x');
+
+		jsdocx.parse({
+			files:[
+				'./webapp/**/*.js',
+				"!webapp/test/**",
+				"!webapp/localService/**"
+			],
+			excludePattern:'(test|localService)',
+			recurse:true,
+			private:true,
+			output:'./webapp/documentation.json',
+			encoding:'utf-8'
+		}, function (err, docs) {
+			if (err) {
+				grunt.log.writeln('JSDoc file generation failed with error:');
+				grunt.log.writeln(err);
+				done(false);
+			} else {
+				grunt.log.writeln('JSDoc file generated');
+				done(true);
+			}			
+		});
+	});
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-openui5");
+    grunt.loadNpmTasks('grunt-ftp-deploy');
 
     // Build task
-    grunt.registerTask("build", [
-        //"openui5_preload", 
-        "copy"
-    ]);
+    grunt.registerTask("build", [ "jsdoc", "openui5_preload",  "copy" ]);
+
     // Default task
-    grunt.registerTask("default", ["clean", "build"]);
+    grunt.registerTask("default", ["clean", "build", "ftp-deploy"]);
 };
